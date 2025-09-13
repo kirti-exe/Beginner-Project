72a443d79d3982ca6daf380e0496a5f8bf3ab09e
kirti-exe <kirti.exe@gmail.com>
2025-09-13 14:37:51 +0530

    Create AudioPlayer.java
    audio player program made in java

---
+++ AudioPlayer.java
@@ -0,0 +1,58 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //Audio player java program
+
+        String filePath = "src\\Tujh-Ko-Dariya-Dil-Ki-Kasam-Jagjit-Singh.wav";  //the audio should be in your src directory 
+        File file = new File(filePath);
+
+        try(Scanner sc = new Scanner(System.in);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(filePath))){
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            String response = "";
+
+            while(!response.equals("Q")){
+
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.print("Enter your choice: ");
+
+                response = sc.next().toUpperCase();
+
+                switch (response){
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice.");
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Could not locate file.");
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file not supported.");
+        }
+        catch (LineUnavailableException e){
+            System.out.println("Unable to access audio resource.");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong.");
+        }
+        finally{
+            System.out.println("Bye!");
+        }
+
+    }
+}
